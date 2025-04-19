d6ef4d74423c35416ef04bb367b2c7674a5f1a11
Welberthy Gustavo <54287781+WelberthyGustavo@users.noreply.github.com>
2025-04-19 19:45:09 +0000

    Matemagos

---
+++ Matemagos/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static int[] nums;
+
+    static void Main()
+    {
+        int quant = int.Parse(Console.ReadLine());
+        nums = new int[quant];
+        LerEntradas(0, quant);
+        ImprimirResultados(0, quant);
+    }
+
+    static void LerEntradas(int idx, int total)
+    {
+        if (idx >= total) return;
+        nums[idx] = int.Parse(Console.ReadLine());
+        LerEntradas(idx + 1, total);
+    }
+
+    static void ImprimirResultados(int idx, int total)
+    {
+        if (idx >= total) return;
+        if (nums[idx] == 0)
+        {
+            Console.WriteLine("0");
+        }
+        else
+        {
+            ConverterParaBinario(nums[idx]);
+            Console.WriteLine();
+        }
+        ImprimirResultados(idx + 1, total);
+    }
+
+    static void ConverterParaBinario(int num)
+    {
+        if (num == 0) return;
+        ConverterParaBinario(num / 2);
+        Console.Write(num % 2);
+    }
+}
